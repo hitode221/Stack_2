f15f85c53e6d2863085c29f62846bcaf681cf963
hitode221 <marina.dr98@mail.ru>
2016-10-31 16:23:13 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -118,7 +118,7 @@ template<class T>
 allocator<T>::allocator(size_t size) : size_(size), count_(0), ptr_((static_cast<T *>(size == 0 ? nullptr : operator new(size * sizeof(T))))), bitset_(size_) {}
 
 template<class T>
-allocator<T>::allocator(allocator const & other) : allocator<T>(other.size()) {
+allocator<T>::allocator(allocator const & other) : allocator<T>(other.size_) {
 	for (size_t i = 0; i < other.size_; ++i) {
 		if (other.bitset_.test(i)) {
 			construct(ptr_ + i, other.get()[i]);
